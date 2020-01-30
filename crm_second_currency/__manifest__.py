5d1e3c5dd9d852c19a2aa7d7a7852975e7c1e9cf
Jhonny Martinez Espinoza <jhon18.26@gmail.com>
2020-01-30 15:52:57 -0500

    commit inicial

---
+++ crm_second_currency/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of SysNeo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "CRM Second Currency",
+    'price': '0',
+    'currency': "USD",
+    "summary": "Adds a second currency to oportunity crm",
+    'description': """
+Second currency
+===============
+Adds a second currency to oportunity.
+    """,
+    "version": "13.0.1.0.0",
+    "license": "Other proprietary",
+    'category': 'Sales/CRM',
+    "author": "SysNeo",
+    "website": "https://sysneo.pe",
+    "depends": ["base", "crm"],
+    "data": [
+        "views/crm_lead_view.xml",
+    ],
+    "demo": [],
+    "installable": True,
+}
